4e3a1e552e1f0b3d5d2a8d259e496fb30e2b2732
Samuel Oliveira <113065988+oliveirasamuel5959@users.noreply.github.com>
2025-10-21 16:15:10 -0300

    Delete 1-POO directory

--- 1-POO/test.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    def __init__(self, status):
-        self.status = status
-
-    def get_status(self):
-        return self.status
-    
-test = Test("Working")
-print(test.get_status())
