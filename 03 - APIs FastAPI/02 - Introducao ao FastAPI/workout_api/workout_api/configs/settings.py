cb9f77ce4a41f9a04037e6412cdb80b9455dbe6f
Samuel-Work <patriciosamuel097@gmail.com>
2025-11-06 10:07:16 -0300

    start project from scratch

--- 03 - APIs FastAPI/02 - Introducao ao FastAPI/workout_api/workout_api/configs/settings.py
+++ 03 - APIs FastAPI/02 - Introducao ao FastAPI/workout_api/workout_api/configs/settings.py
@@ -4,5 +4,4 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
 
-
 settings = Settings()
