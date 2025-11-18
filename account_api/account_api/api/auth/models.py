f87a200fbd92c206bd117d1a73a374db29627427
Samuel-Work <patriciosamuel097@gmail.com>
2025-11-18 08:00:21 -0300

    add token models

--- account_api/account_api/api/auth/models.py
+++ account_api/account_api/api/auth/models.py
@@ -12,11 +12,3 @@ class TokenModel(Base):
     refresh_token: Mapped[str] = mapped_column(String(450), nullable=False)
     status: Mapped[bool] = mapped_column(Boolean, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-
-# class TokenTable(Base):
-#     __tablename__ = "token"
-#     user_id = Column(Integer)  # Foreign key reference to associate token with a specific user ID
-#     access_toke = Column(String(450), primary_key=True)  # Primary key column for the access token, max length 450
-#     refresh_toke = Column(String(450), nullable=False)  # Column for the refresh token, max length 450, cannot be null
-#     status = Column(Boolean)  # Column to store the status of the token (e.g., active or inactive)
-#     created_date = Column(DateTime, default=datetime.datetime.now) 
